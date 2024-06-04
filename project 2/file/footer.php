8fb1beadccd74e2694c76093ef2b71d85f1c9a32
Khalil <ps250618@summacollege.nl>
2024-06-04 14:35:52 +0200

    footer css en php

--- project 2/file/footer.php
+++ project 2/file/footer.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,9 +13,12 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <title>footer</title>
 </head>
+
 <body>
     <footer>
-    
+        <p>Neutraal Kieslab ® 2024. Alle rechten voorbehouden.</p>
+        <a href="privacy.php">privacy</a>
     </footer>
 </body>
+
 </html>
