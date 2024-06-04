70b3e0f11f38065fcd39eaac7c1f9917f1baedbc
Remi <ps252727@summacollege.nl>
2024-06-04 13:57:20 +0200

    header

--- project 2/file/header.php
+++ project 2/file/header.php
@@ -4,18 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/header.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
 <header> 
-    <h1 name="title"> Neutraal Kieslab </h1>
+    <h1> Neutraal Kieslab </h1>
 
-    <div class="container">
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-    </div>
+
    
 </header>
 </body>
