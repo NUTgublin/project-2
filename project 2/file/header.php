5615ce0175d629ab7f25f53002b019ca7af6486e
Khalil <ps250618@summacollege.nl>
2024-06-04 13:41:43 +0200

    css gelinked

--- project 2/file/header.php
+++ project 2/file/header.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/header.css">
+    <title>Document</title>
+</head>
+<body>
 <header> 
     <h1 name="title"> F1 Website </h1>
 
@@ -9,3 +18,5 @@
     </div>
    
 </header>
+</body>
+</html>
--- project 2/file/index.php
+++ project 2/file/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<?php require 'header.php'?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,7 +15,7 @@
 </head>
 
 <body>
-    <?php require 'header.php'?>
+  
 
 </body>
 
