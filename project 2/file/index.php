f2ed34466809f6444add34cf4420ff5edefcfab4
Khalil <ps250618@summacollege.nl>
2024-06-04 13:31:52 +0200

    heb fonts gezet voor de teksten

--- project 2/file/index.php
+++ project 2/file/index.php
@@ -1,15 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Neutraal Kieslab</title>
     <link rel="stylesheet" href="../css/index.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
 </head>
+
 <body>
-     <h1>ieieieiei</h1>
+    <h3>neutraal Kieslab</h3>
+    <h1>ieieieiei</h1>
 </body>
+
 </html>
