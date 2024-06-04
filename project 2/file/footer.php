00396b4db85f942128375a8bc086293f87ede2f8
Khalil <ps250618@summacollege.nl>
2024-06-04 13:57:46 +0200

    2

--- project 2/file/footer.php
+++ project 2/file/footer.php
@@ -1,22 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php require 'header.php'?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Neutraal Kieslab</title>
-    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/footer.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+    <title>footer</title>
 </head>
-
 <body>
-  
-<?php require 'footer.php' ?>
-</body>
+    <footer>
     
+    </footer>
+</body>
 </html>
