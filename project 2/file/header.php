bc65621999c47fe8b39adc3685feacb82152909a
Remi <ps252727@summacollege.nl>
2024-06-04 13:42:14 +0200

    iji

--- project 2/file/header.php
+++ project 2/file/header.php
@@ -1,11 +1,11 @@
 <header> 
-    <h1 name="title"> F1 Website </h1>
+    <h1 name="title"> Neutraal Kieslab </h1>
 
     <div class="container">
-        <p>Home</p>
-        <p>Coureurs</p>
-        <p>Rankings</p>
-        <p>Contact</p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
     </div>
    
 </header>
