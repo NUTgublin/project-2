1a617196b678c37217202aa6165e7b902d2c85b7
Mohamad <mohamad.altalla17@gmail.com>
2024-06-05 14:59:27 +0200

    Alles goed gelinked

---
+++ project 2/file/nieuws.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php require 'header.php' ?>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>stellingen</title>
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/nieuws.css">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+<nav>
+     
+        <a href="index.php">home</a>
+
+        <a href="partijen.php">partijen</a>
+        <a href="nieuws.php">nieuws</a>
+        <a href="stellingen.php">stellingen</a>
+    </nav>
+    <div class="GridContainer">
+
+    </div>
+    <?php require 'footer.php' ?>
+</body>
+</html>
--- project 2/file/stellingen.php
+++ project 2/file/stellingen.php
@@ -5,8 +5,10 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>stellingen</title>
     <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/stellingen.css">
+
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
