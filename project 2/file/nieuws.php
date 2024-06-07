34c08a336e5522e890632e61f7b3e1167d8cfdff
Mohamad <mohamad.altalla17@gmail.com>
2024-06-07 12:00:48 +0200

    Nieuws

--- project 2/file/nieuws.php
+++ project 2/file/nieuws.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php require 'header.php' ?>
-
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>stellingen</title>
-    <link rel="stylesheet" href="../css/index.css">
-    <link rel="stylesheet" href="../css/nieuws.css">
-
+  <?php require 'header.php' ?>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>stellingen</title>
+  <link rel="stylesheet" href="../css/index.css">
+  <link rel="stylesheet" href="../css/nieuws.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
@@ -17,21 +15,37 @@
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
 </head>
 <body>
-<nav>
-     
-        <a href="index.php">home</a>
+  <nav>
+    <a href="index.php">home</a>
+    <a href="partijen.php">partijen</a>
+    <a href="nieuws.php">nieuws</a>
+    <a href="stellingen.php">stellingen</a>
+  </nav>
+  <div class="GridContainer">
+    <div class="parentDiv">
+      <div class="firstDiv">
+        <h3>GroenLinks-PvdA: 'Economie gaat kapot met verdeeld Europa' • Lijsttrekkers van andere partijen aan het woord</h3>
+      </div>
+      <div class="SecondDiv">
+        <p>Op de avond voor de Europese verkiezingen gingen negen lijsttrekkers met elkaar in debat in NOS Europa kiest: Het debat. In dit liveblog lees je de belangrijkste punten uit het debat terug.</p>
+        <ul>
+          <li>De stembureaus zijn morgen open van 7.30 tot 21.00 uur.</li>
+          <li>Samen kiezen we wie de 31 Nederlandse zetels in het Europees Parlement gaan innemen.</li>
+          
+        </ul>
+        <p>Je stemt in je eigen land op iemand die lid wil worden van het Europees Parlement (EP). Hoe groter het land, hoe meer Europarlementariërs het mag afvaardigen. Nederland heeft er 31, maar Spanje bijvoorbeeld 59 en Duitsland 96.
 
-        <a href="partijen.php">partijen</a>
-        <a href="nieuws.php">nieuws</a>
-        <a href="stellingen.php">stellingen</a>
-    </nav>
-    <div class="GridContainer">
-<h2 class="Hallo">GroenLinks-PvdA: 'Economie gaat kapot met verdeeld Europa' • Lijsttrekkers van andere partijen aan het woord<h2>
-  <p>Op de avond voor de Europese verkiezingen gingen negen lijsttrekkers met elkaar in debat in NOS Europa kiest: Het debat. In dit liveblog lees je de belangrijkste punten uit het debat terug.
-    <ul>
-<li>De stembureaus zijn morgen open van 7.30 tot 21.00 uur.</li>
-<li>Samen kiezen we wie de 31 Nederlandse zetels in het Europees Parlement gaan innemen.</p></li></ul>
+</p>
+      </div>
     </div>
-    <?php require 'footer.php' ?>
+    <div class="secondColumn">
+      <div class="thirdDiv">
+      </div>
+      <div class="fourthDiv">
+        
+      </div>
+    </div>
+  </div>
+  <?php require 'footer.php' ?>
 </body>
 </html>
