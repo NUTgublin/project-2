f23058e5cd26df31c227f1f813d1d5fe1887cb30
Khalil <ps250618@summacollege.nl>
2024-06-11 14:12:21 +0200

    footer gefixt

--- project 2/file/nieuws.php
+++ project 2/file/nieuws.php
@@ -1,69 +1,96 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <?php require 'header.php' ?>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>stellingen</title>
-    <link rel="stylesheet" href="../css/index.css">
-    <link rel="stylesheet" href="../css/nieuws.css">
-   
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+  <?php require 'header.php' ?>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>stellingen</title>
+  <link rel="stylesheet" href="../css/index.css">
+  <link rel="stylesheet" href="../css/nieuws.css">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
 </head>
+
 <body>
-    <nav>
-        <a href="index.php">home</a>
-        <a href="partijen.php">partijen</a>
-        <a href="nieuws.php">nieuws</a>
-        <a href="stellingen.php">stellingen</a>
-    </nav>
+  <nav>
+    <a href="index.php">home</a>
+    <a href="partijen.php">partijen</a>
+    <a href="nieuws.php">nieuws</a>
+    <a href="stellingen.php">stellingen</a>
+  </nav>
   <div class="GridContainer">
     <div class="contentDiv parentDiv">
-    <img src="..\Logos\groenlinks.jpg" alt="GroenLinks-PvdA Logo">
-      <h3>GroenLinks-PvdA: 'Economie gaat kapot met verdeeld Europa' • Lijsttrekkers van andere partijen aan het woord</h3>
-      <p>Op de avond voor de Europese verkiezingen gingen negen lijsttrekkers met elkaar in debat in NOS Europa kiest: Het debat. In dit liveblog lees je de belangrijkste punten uit het debat terug.</p>
+      <img src="..\Logos\groenlinks.jpg" alt="GroenLinks-PvdA Logo">
+      <h3>GroenLinks-PvdA: 'Economie gaat kapot met verdeeld Europa' • Lijsttrekkers van andere partijen aan het woord
+      </h3>
+      <p>Op de avond voor de Europese verkiezingen gingen negen lijsttrekkers met elkaar in debat in NOS Europa kiest:
+        Het debat. In dit liveblog lees je de belangrijkste punten uit het debat terug.</p>
       <ul>
         <li>De stembureaus zijn morgen open van 7.30 tot 21.00 uur.</li>
         <li>Samen kiezen we wie de 31 Nederlandse zetels in het Europees Parlement gaan innemen.</li>
       </ul>
-      <p>Je stemt in je eigen land op iemand die lid wil worden van het Europees Parlement (EP). Hoe groter het land, hoe meer Europarlementariërs het mag afvaardigen. Nederland heeft er 31, maar Spanje bijvoorbeeld 59 en Duitsland 96.</p>
+      <p>Je stemt in je eigen land op iemand die lid wil worden van het Europees Parlement (EP). Hoe groter het land,
+        hoe meer Europarlementariërs het mag afvaardigen. Nederland heeft er 31, maar Spanje bijvoorbeeld 59 en
+        Duitsland 96.</p>
     </div>
     <div class="contentDiv secondColumn">
-    <img src="..\Logos\hacker.jpg" alt="Hacker Image">
+      <img src="..\Logos\hacker.jpg" alt="Hacker Image">
       <h3>Websites politieke partijen aangevallen door hackers</h3>
-      <p>Verschillende politieke partijen melden dat hun websites zijn aangevallen door hackers. De sites zijn of waren slecht bereikbaar door DDoS-aanvallen. Dat was hinderlijk voor kiezers die vandaag nog informatie wilden zoeken over kandidaten voor de Europese verkiezingen en hun standpunten.</p>
+      <p>Verschillende politieke partijen melden dat hun websites zijn aangevallen door hackers. De sites zijn of waren
+        slecht bereikbaar door DDoS-aanvallen. Dat was hinderlijk voor kiezers die vandaag nog informatie wilden zoeken
+        over kandidaten voor de Europese verkiezingen en hun standpunten.</p>
       <ul>
-        <li>Het CDA meldt op X: "Vanochtend was de CDA-website door een zware DDoS-aanval tijdelijk minder bereikbaar. Op verkiezingsdag beschouwen we dit als een aanval op vrije, democratische verkiezingen. We maken melding bij de relevante instanties."</li>
-        <li>Ook de sites van PVV en FvD lagen er tijdelijk uit. FvD-leider Baudet zegt dat "het hele team overuren werkt om de aanval af te slaan." Hij voegt er met een smiley aan toe: "Russen?"</li>
+        <li>Het CDA meldt op X: "Vanochtend was de CDA-website door een zware DDoS-aanval tijdelijk minder bereikbaar.
+          Op verkiezingsdag beschouwen we dit als een aanval op vrije, democratische verkiezingen. We maken melding bij
+          de relevante instanties."</li>
+        <li>Ook de sites van PVV en FvD lagen er tijdelijk uit. FvD-leider Baudet zegt dat "het hele team overuren werkt
+          om de aanval af te slaan." Hij voegt er met een smiley aan toe: "Russen?"</li>
         <li>Pro-Russisch hackers</li>
-        <li>De aanvallen zouden inderdaad uit die hoek kunnen komen, meldt het ANP, namelijk van de hackers van het pro-Russische Hacknet. "We gaan door met aanvallen op de websites van Nederland en hun partijen", schrijft de groep op Telegram.</li>
-        <li>Bij een DDoS-aanval (Distributed Denial of Service) worden computerservers bestookt met grote hoeveelheden dataverkeer, waardoor de servers verstopt raken en geen ruimte meer hebben voor gewoon internetverkeer. </li>
+        <li>De aanvallen zouden inderdaad uit die hoek kunnen komen, meldt het ANP, namelijk van de hackers van het
+          pro-Russische Hacknet. "We gaan door met aanvallen op de websites van Nederland en hun partijen", schrijft de
+          groep op Telegram.</li>
+        <li>Bij een DDoS-aanval (Distributed Denial of Service) worden computerservers bestookt met grote hoeveelheden
+          dataverkeer, waardoor de servers verstopt raken en geen ruimte meer hebben voor gewoon internetverkeer. </li>
       </ul>
     </div>
     <div class="contentDiv thirdDiv">
       <h3>GL-PvdA met acht zetels grootste, PVV tweede partij met zes zetels</h3>
-      <p>GroenLinks-PvdA is bij de Europese verkiezingen in Nederland de grootste partij geworden. Volgens de definitieve prognose die zondag om 23.00 uur bekend werd gemaakt, krijgt GL-PvdA 8 zetels in het Europees Parlement. De PVV van Geert Wilders is de grote winnaar en gaat van 1 naar 6. Dat is er wel één minder dan in de exitpoll van donderdag van Ipsos I&O. </p>
+      <p>GroenLinks-PvdA is bij de Europese verkiezingen in Nederland de grootste partij geworden. Volgens de
+        definitieve prognose die zondag om 23.00 uur bekend werd gemaakt, krijgt GL-PvdA 8 zetels in het Europees
+        Parlement. De PVV van Geert Wilders is de grote winnaar en gaat van 1 naar 6. Dat is er wel één minder dan in de
+        exitpoll van donderdag van Ipsos I&O. </p>
     </div>
     <div class="contentDiv fourthDiv">
-    <img src="..\Logos\vape.jpg" alt="Vape Image">
+      <img src="..\Logos\vape.jpg" alt="Vape Image">
       <h3>Neutrale verpakking ook verplicht voor vapes en sigaren</h3>
-      <p>Vanaf volgend jaar mogen vapes en sigaren alleen nog in een neutrale verpakking worden verkocht. Zo'n verplichting geldt sinds 2020 al voor sigaretten en shag, maar het kabinet wil dat uitbreiden.</p>
+      <p>Vanaf volgend jaar mogen vapes en sigaren alleen nog in een neutrale verpakking worden verkocht. Zo'n
+        verplichting geldt sinds 2020 al voor sigaretten en shag, maar het kabinet wil dat uitbreiden.</p>
     </div>
     <div class="contentDiv fifthDiv">
-    <img src="..\Logos\Rechts.jpg" alt="Right Wing Image">
+      <img src="..\Logos\Rechts.jpg" alt="Right Wing Image">
       <h3>Radicaal-rechts wint terrein in EU, maar 'niet genoeg voor rechtsere koers'</h3>
-      <p>De uiterst eurokritische fracties winnen flink terrein in het Europees Parlement. Onder meer in Frankrijk en Duitsland hebben radicaal-rechtse partijen veel kiezers getrokken. Maar dat is niet genoeg om in het parlement een rechtsere koers te kunnen afdwingen, zegt Theresa Kuhn, hoogleraar Europese Studies aan de Universiteit van Amsterdam.</p>
-      <p>De middenpartijen in het Europees Parlement hebben nog steeds een meerderheid. De christendemocratische Europese Volkspartij (EVP) vormt opnieuw de grootste fractie, met 184 van de 720 zetels. EVP heeft de rechtse partijen dus niet nodig, zegt Kuhn in het NOS Radio 1 Journaal. "Samen met de sociaaldemocraten en de liberalen kunnen ze nog steeds een ruime meerderheid vormen."</p>
-      <p>Het verlies van pro-Europese partijen zit bij de liberalen (Renew) en de Groenen. "Dat maakt ze niet meer zo sterk. Daarbij komt ook nog dat een van de bekendste leden van Renew, De Franse president Macron, ook in een zwakke positie staat."
-         Na de grote zege van radicaal-rechts (Marine Le Pen) bij de Europese verkiezingen heeft Macron zijn nationale parlement ontbonden en parlementsverkiezingen 
-          </P>
+      <p>De uiterst eurokritische fracties winnen flink terrein in het Europees Parlement. Onder meer in Frankrijk en
+        Duitsland hebben radicaal-rechtse partijen veel kiezers getrokken. Maar dat is niet genoeg om in het parlement
+        een rechtsere koers te kunnen afdwingen, zegt Theresa Kuhn, hoogleraar Europese Studies aan de Universiteit van
+        Amsterdam.</p>
+      <p>De middenpartijen in het Europees Parlement hebben nog steeds een meerderheid. De christendemocratische
+        Europese Volkspartij (EVP) vormt opnieuw de grootste fractie, met 184 van de 720 zetels. EVP heeft de rechtse
+        partijen dus niet nodig, zegt Kuhn in het NOS Radio 1 Journaal. "Samen met de sociaaldemocraten en de liberalen
+        kunnen ze nog steeds een ruime meerderheid vormen."</p>
+      <p>Het verlies van pro-Europese partijen zit bij de liberalen (Renew) en de Groenen. "Dat maakt ze niet meer zo
+        sterk. Daarbij komt ook nog dat een van de bekendste leden van Renew, De Franse president Macron, ook in een
+        zwakke positie staat."
+        Na de grote zege van radicaal-rechts (Marine Le Pen) bij de Europese verkiezingen heeft Macron zijn nationale
+        parlement ontbonden en parlementsverkiezingen.</p>
     </div>
   </div>
   <?php require 'footer.php' ?>
 </body>
+
 </html>
